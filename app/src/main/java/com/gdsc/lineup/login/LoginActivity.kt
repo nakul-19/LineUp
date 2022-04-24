509bc05fca7d1356f6c18e6ae67fa7eb209f26da
Nakul Sharma <rnqnkl@gmail.com>
2022-04-24 22:44:06 +0530

    Added Login Activity

---
+++ app/src/main/java/com/gdsc/lineup/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.gdsc.lineup.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gdsc.lineup.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
