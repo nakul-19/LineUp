43ac482f2bc4d009721892114d6f9dabc667a6b8
dhruvbaliyan <dhruvbaliyan1001@gmail.com>
2022-04-26 06:43:59 +0530

    Completed Scanner Screen Scan Functionality

--- app/src/main/java/com/gdsc/lineup/interfaces/QRCodeFoundListener.kt
+++
@@ -1,6 +0,0 @@
-package com.gdsc.lineup.interfaces
-
-interface QRCodeFoundListener {
-    fun onQRCodeFound(qrCode: String)
-    fun qrCodeNotFound()
-}
--- app/src/main/java/com/gdsc/lineup/main/ScannerFragment.kt
+++ app/src/main/java/com/gdsc/lineup/main/ScannerFragment.kt
@@ -1,34 +1,31 @@
 package com.gdsc.lineup.main
 
 import android.Manifest
+import android.app.Dialog
 import android.content.pm.PackageManager
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import android.util.Size
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.camera.core.CameraSelector
-import androidx.camera.core.ImageAnalysis
-import androidx.camera.core.Preview
-import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.camera.view.PreviewView
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.LifecycleOwner
+import com.budiyev.android.codescanner.*
+import com.gdsc.lineup.databinding.DialogTeamMemberFoundBinding
 import com.gdsc.lineup.databinding.FragmentScannerBinding
-import com.gdsc.lineup.interfaces.QRCodeFoundListener
-import com.gdsc.lineup.utils.QRCodeImageAnalyzer
-import com.google.common.util.concurrent.ListenableFuture
-import java.util.concurrent.ExecutionException
 
 
 class ScannerFragment : Fragment() {
 
     private lateinit var binding: FragmentScannerBinding
-    private lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
     private lateinit var scannedQR: String
+    private lateinit var codeScanner: CodeScanner
 
     private val requestPermissionLauncher = registerForActivityResult(
         ActivityResultContracts.RequestPermission()
@@ -51,8 +48,56 @@ class ScannerFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext());
+        setupCodeScanner()
         requestCamera()
+    }
+
+    private fun setupCodeScanner() {
+        codeScanner = CodeScanner(requireContext(), binding.scannerView)
+        codeScanner.apply {
+            camera = CodeScanner.CAMERA_BACK
+            formats = CodeScanner.TWO_DIMENSIONAL_FORMATS
+            autoFocusMode = AutoFocusMode.SAFE
+            isAutoFocusEnabled = true
+            isFlashEnabled = false
+
+            decodeCallback = DecodeCallback {
+                Handler(Looper.getMainLooper()).post {
+                    handleQR(it.text)
+                }
+            }
+
+            errorCallback = ErrorCallback {
+                Log.e("Code Scanner", it.message.toString())
+            }
+        }
+    }
+
+    // this function receives
+    private fun handleQR(id: String) {
+
+        // make api call to check if the user is in the same team or not
+        // and show the dialog box accordingly showTeamMemberFoundDialog() or showNonTeamMemberFoundDialog()
+        showTeamMemberFoundDialog()
+
+    }
+
+    private fun showTeamMemberFoundDialog() {
+
+        val view = DialogTeamMemberFoundBinding.inflate(layoutInflater)
+        val dialog = Dialog(requireContext())
+
+        dialog.setContentView(view.root)
+        view.cross.setOnClickListener { dialog.hide() }
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        dialog.show()
+
+    }
+
+    private fun showNonTeamMemberFoundDialog() {
+        // no designs for this case yet (Can show toast for now)
+        Toast.makeText(requireContext(), "Different Team Member", Toast.LENGTH_LONG).show()
     }
 
     private fun requestCamera() {
@@ -70,55 +115,12 @@ class ScannerFragment : Fragment() {
     }
 
     private fun startCamera() {
-        cameraProviderFuture.addListener({
-            try {
-                val cameraProvider = cameraProviderFuture.get()
-                bindCameraPreview(cameraProvider)
-            } catch (e: ExecutionException) {
-                Toast.makeText(requireContext(), "Error starting camera " + e.message, Toast.LENGTH_SHORT)
-                    .show()
-            } catch (e: InterruptedException) {
-                Toast.makeText(requireContext(), "Error starting camera " + e.message, Toast.LENGTH_SHORT)
-                    .show()
-            }
-        }, ContextCompat.getMainExecutor(requireContext()))
+        codeScanner.startPreview()
     }
 
-    private fun bindCameraPreview(cameraProvider: ProcessCameraProvider) {
-
-        binding.previewView.preferredImplementationMode = PreviewView.ImplementationMode.SURFACE_VIEW
-        val preview = Preview.Builder()
-            .build()
-
-        val cameraSelector = CameraSelector.Builder()
-            .requireLensFacing(CameraSelector.LENS_FACING_BACK)
-            .build()
-
-        preview.setSurfaceProvider(binding.previewView.createSurfaceProvider())
-
-        val imageAnalysis = ImageAnalysis.Builder()
-            .setTargetResolution(Size(512, 512))
-            .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
-            .build()
-
-        imageAnalysis.setAnalyzer(
-            ContextCompat.getMainExecutor(requireContext()),
-            QRCodeImageAnalyzer(object : QRCodeFoundListener {
-                override fun onQRCodeFound(qrCode: String) {
-                    scannedQR = qrCode
-                    Toast.makeText(requireContext(), scannedQR, Toast.LENGTH_LONG).show()
-                }
-                override fun qrCodeNotFound() {
-                    // TODO
-                }
-            })
-        )
-        cameraProvider.bindToLifecycle(
-            (this as LifecycleOwner),
-            cameraSelector,
-            imageAnalysis,
-            preview
-        )
+    override fun onPause() {
+        super.onPause()
+        codeScanner.releaseResources()
     }
 
 }
